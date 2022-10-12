dcc111e9a872af68dfb2b581096d787e39a8cd22
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-10-12 16:52:02 +0200

    refacto(layout): update layout component and add navigation bar in sidebar component

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,13 +1,11 @@
 import Header from "./LayoutComponents/Header"
 import Sidebar from "./LayoutComponents/Sidebar"
 import styles from "./Layout.module.css"
-import { useState } from "react"
 
 function Layout({ children }) {
-    const [isOpened, setIsOpened] = useState(false);
     return (
         <div className={styles.layout}>
-            <Sidebar isOpened={isOpened} setIsOpened={setIsOpened} />
+            <Sidebar />
             <div className={styles.container}>
                 <Header />
                 <main className={styles.content}>{children}</main>
--- components/Layout/LayoutComponents/Sidebar.jsx
+++ components/Layout/LayoutComponents/Sidebar.jsx
@@ -1,18 +1,33 @@
 import styles from "../Layout.module.css"
+import { Link } from "react-router-dom"
+// import { GrMoney } from 'react-icons/all'
+import { FcAreaChart } from 'react-icons/all'
+import { MdOutlineDashboard } from 'react-icons/all'
+import { BsBarChartLine } from 'react-icons/all'
+import { TbZoomMoney } from 'react-icons/all'
+import { BiWallet } from 'react-icons/all'
+import { HiOutlineNewspaper } from 'react-icons/all'
+import { IoSettingsOutline } from 'react-icons/all'
+import { IoLogOutOutline } from 'react-icons/all'
 
 function Sidebar(props) {
-    const { isOpened, setIsOpened } = props
-
+Link
     return (
-        <aside className={`${styles.sidebar} ${isOpened ? styles.opened : styles.drawer}`}>
-            <div className={styles.icon} onClick={() => setIsOpened(!isOpened)}>
-                {isOpened ? <a href="#">Close</a> : <a href="#">Open</a>}
-            </div>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
+        <aside className={styles.sidebar}>
+            <div className={styles.logo}><FcAreaChart /><span>INVESTAPP</span></div>
+            <nav className={styles.navbar}>
+                <ul>
+                    <li><Link to={"/overview"}><MdOutlineDashboard /><span>Overview</span></Link></li>
+                    <li><Link to={"/investments"}><BsBarChartLine /><span>Investments</span></Link></li>
+                    <li><Link to={"/stocks"}><TbZoomMoney /><span>Stocks</span></Link></li>
+                    <li><Link to={"/wallet"}><BiWallet /><span>Wallet</span></Link></li>
+                    <li><Link to={"/news"}><HiOutlineNewspaper /><span>News</span></Link></li>
+                </ul>
+                <ul>
+                    <li><Link to={"/settings"}><IoSettingsOutline /><span>Settings</span></Link></li>
+                    <li><Link to={"/logout"}><IoLogOutOutline /><span>Logout</span></Link></li>
+                </ul>
+            </nav>
         </aside>
     )
 }
