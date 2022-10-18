dc1b0aa50e9c189524572395276436ae07edfca4
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-10-18 20:23:26 +0200

    refactor(layout): move components inside src folder

--- src/components/Layout/LayoutComponents/Sidebar.jsx
+++ src/components/Layout/LayoutComponents/Sidebar.jsx
@@ -1,17 +1,19 @@
 import styles from "../Layout.module.css"
 import { Link } from "react-router-dom"
 // import { GrMoney } from 'react-icons/all'
-import { FcAreaChart } from 'react-icons/all'
-import { MdOutlineDashboard } from 'react-icons/all'
-import { BsBarChartLine } from 'react-icons/all'
-import { TbZoomMoney } from 'react-icons/all'
-import { BiWallet } from 'react-icons/all'
-import { HiOutlineNewspaper } from 'react-icons/all'
-import { IoSettingsOutline } from 'react-icons/all'
-import { IoLogOutOutline } from 'react-icons/all'
+import {
+    FcAreaChart,
+    MdOutlineDashboard,
+    BsBarChartLine,
+    TbZoomMoney,
+    BiWallet,
+    HiOutlineNewspaper,
+    IoSettingsOutline,
+    IoLogOutOutline
+} from 'react-icons/all'
 
 function Sidebar(props) {
-Link
+    Link
     return (
         <aside className={styles.sidebar}>
             <div className={styles.logo}><FcAreaChart /><span>INVESTAPP</span></div>
