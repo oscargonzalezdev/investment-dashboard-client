2fb7bb460cbadf5e153a879e56069cfa7676e83a
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-10-19 22:49:49 +0200

    feat(App): add routes using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-function App() {
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Layout from "../src/components/Layout/Layout"
+import News from "../src/components/News/News"
+import StockDetails from "../src/components/StockDetails/StockDetails"
+import StockList from '../src/components/StocksList/StoksList'
+import Wallet from "./components/Wallet/Wallet"
 
+function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path={'/overview'} />
+          <Route path={'/investments'} />
+          <Route path={'/stocks'} element={<StockList />} />
+          <Route path={'/stocks/:symbol'} element={<StockDetails />} />
+          <Route path={'/wallet'} element={<Wallet />} />
+          <Route path={'/news'} element={<News />} />
+          <Route path={'/settings'} />
+          <Route path={'/logout'} />
+        </Routes>
+      </Layout>
+    </Router>
   )
 }
 
