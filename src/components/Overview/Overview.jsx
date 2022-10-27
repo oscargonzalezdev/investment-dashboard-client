f263987ac8a0033fa4461337ad08a213b6441efa
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-10-27 20:44:09 +0100

    feat(overview): add overview component to display a selection of stock, news and chart components

---
+++ src/components/Overview/Overview.jsx
@@ -0,0 +1,32 @@
+import { Box, Grid, GridItem, Button, ButtonGroup, Heading } from "@chakra-ui/react"
+import { useContext } from "react"
+import { DataContext } from "../../context/data.context"
+import Balance from "../Charts/Balance"
+import PieChart from "../Charts/PieChart"
+import BreakingNews from "../News/BreakingNews"
+import StockDetails from "../Stocks/StocksComponents/StockDetails"
+import StockTable from "../Stocks/StocksComponents/StockTable"
+
+function Overview() {
+    const { trendingStocks } = useContext(DataContext)
+
+    return (
+        <Box className="main-container">
+            <Grid templateColumns='repeat(9, 1fr)' gap='20px'>
+                <GridItem display='flex' flexDirection='column' gap='20px' colSpan={5}>
+                    <StockDetails w='100%' title='Stock of the Week' symbol2={'IBM'} />
+                    <StockTable w='100%' h='400px' data={trendingStocks} type='trending' title='Trending Stocks' />
+                </GridItem>
+                <GridItem display='flex' flexDirection='column' gap='20px' colSpan={4}>
+                    <Box display='flex' gap='20px'>
+                        <Balance />
+                        <PieChart />
+                    </Box>
+                    <BreakingNews h='620px' />
+                </GridItem>
+            </Grid>
+        </Box>
+    )
+}
+
+export default Overview
