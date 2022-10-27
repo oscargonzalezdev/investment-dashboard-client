1bfb8714f99d3a15268ab4134382b93ebaf57df1
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-10-27 20:15:13 +0100

    feat(news): add news component and styles for it

---
+++ src/components/News/BreakingNews.jsx
@@ -0,0 +1,44 @@
+import { useContext } from 'react'
+import { Box, Grid, GridItem, Heading } from '@chakra-ui/react'
+import styles from './News.module.css'
+import { DataContext } from '../../context/data.context'
+
+function BreakingNews({w, h}) {
+    const { news } = useContext(DataContext)
+    return (
+        <>
+            {
+                !news.length > 0
+                    ? <p>Loading...</p>
+                    : <Box className='box' w={w ? w : 'fit-content'}>
+                        <Heading className='section-title'>Breaking News</Heading>
+                        <Box className={styles.containerLatestNews}>
+                            <Grid
+                                className={styles.boxGrid}
+                                templateRows='repeat(6, 1fr)'
+                                templateColumns='repeat(1, 1fr)'
+                                h={h ? h : 'fit-content'}
+                                gap='10px'
+                            >
+                                {
+                                    news.slice(0, 15).map((news, index) => {
+                                        return (
+                                            <GridItem key={index} colSpan={1} bg={news.multimedia === null ? 'rgba(0, 0, 0, 0.15)' : `url(${news.multimedia[0].url})`} bgRepeat="no-repeat" bgSize='cover' bgPosition='center' borderRadius={8}>
+                                                <a href={news.short_url} target="_blank" rel="noopener noreferrer" style={{ width: '100%' }}>
+                                                    <Box h='150px' backgroundColor='rgba(0, 0, 0, 0.7)' display='flex' alignItems='center' textAlign='left' p='20px' borderRadius={8} >
+                                                        <Heading as='h3' color='#fff' >{news.title}</Heading>
+                                                    </Box>
+                                                </a>
+                                            </GridItem>
+                                        )
+                                    })
+                                }
+                            </Grid>
+                        </Box>
+                    </Box>
+            }
+        </>
+    )
+}
+
+export default BreakingNews
---
+++ src/components/News/News.jsx
@@ -0,0 +1,48 @@
+import { useContext } from 'react'
+import { Box, Grid, GridItem, Heading } from '@chakra-ui/react'
+import styles from './News.module.css'
+import { DataContext } from '../../context/data.context'
+import BreakingNews from './BreakingNews'
+
+function News() {
+    const { news } = useContext(DataContext)
+    return (
+        <Box className='main-container'>
+            {
+                !news.length > 0
+                    ? <p>Loading...</p>
+                    : (
+                        <Box className={styles.containerNews}>
+                            <Box bgColor='var(--color-bg-box-light)' padding={30} borderRadius={8} shadow='1px 1px 2px #ccc'>
+                                <Heading textAlign='left' marginBottom={30}>Top News</Heading>
+                                <Grid
+                                    className={styles.boxGrid}
+                                    templateRows='repeat(2, 1fr)'
+                                    templateColumns='repeat(2, 1fr)'
+                                    h='700px'
+                                    gap='10px'
+                                >
+                                    {
+                                        news.slice(0, 3).map((news, index) => {
+                                            return (
+                                                <GridItem className='styles.gridTopNews' key={index} borderRadius={8} colSpan={index === 0 ? 2 : 1} bg={news.multimedia === null ? 'rgba(0, 0, 0, 0.15)' : `url(${news.multimedia[0].url})`} bgRepeat="no-repeat" bgSize='cover' bgPosition='center' display='flex' alignItems='end' textAlign='left'>
+                                                    <a href={news.short_url} target="_blank" rel="noopener noreferrer" style={{ width: '100%' }}>
+                                                        <Box borderRadius='0 0 8px 8px' backgroundColor='rgba(0, 0, 0, 0.70)' p='20px'>
+                                                            <Heading as='h3' color='#fff'>{news.title}</Heading>
+                                                        </Box>
+                                                    </a>
+                                                </GridItem>
+                                            )
+                                        })
+                                    }
+                                </Grid>
+                            </Box>
+                            <BreakingNews h='700px' w='300px' />
+                        </Box>
+                    )
+            }
+        </Box>
+    )
+}
+
+export default News
