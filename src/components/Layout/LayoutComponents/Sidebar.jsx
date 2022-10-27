f4000b2e3f9e4a58bf1e33a24e125ff0f21196b5
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-10-27 20:08:44 +0100

    refactor(layout): update sidebar component, add function to handle logout

--- src/components/Layout/LayoutComponents/Sidebar.jsx
+++ src/components/Layout/LayoutComponents/Sidebar.jsx
@@ -1,6 +1,6 @@
 import styles from "../Layout.module.css"
 import { Link } from "react-router-dom"
-// import { GrMoney } from 'react-icons/all'
+
 import {
     FcAreaChart,
     MdOutlineDashboard,
@@ -11,23 +11,28 @@ import {
     IoSettingsOutline,
     IoLogOutOutline
 } from 'react-icons/all'
+import Logo from "../../Logo/Logo"
+import { Box, Button } from "@chakra-ui/react"
 
 function Sidebar(props) {
-    Link
+    const handleLogOut = () => {
+        return window.location.reload()
+    }
     return (
         <aside className={styles.sidebar}>
-            <div className={styles.logo}><FcAreaChart /><span>INVESTAPP</span></div>
+            <Box className={styles.logoResponsive}><FcAreaChart /></Box>
+            <Box className={styles.logo} ><Logo size='1.5rem' /></Box>
             <nav className={styles.navbar}>
                 <ul>
-                    <li><Link to={"/overview"}><MdOutlineDashboard /><span>Overview</span></Link></li>
-                    <li><Link to={"/investments"}><BsBarChartLine /><span>Investments</span></Link></li>
+                    <li><Link to={"/"}><MdOutlineDashboard /><span>Overview</span></Link></li>
                     <li><Link to={"/stocks"}><TbZoomMoney /><span>Stocks</span></Link></li>
+                    <li><Link to={"/investments"}><BsBarChartLine /><span>Investments</span></Link></li>
                     <li><Link to={"/wallet"}><BiWallet /><span>Wallet</span></Link></li>
                     <li><Link to={"/news"}><HiOutlineNewspaper /><span>News</span></Link></li>
                 </ul>
                 <ul>
-                    <li><Link to={"/settings"}><IoSettingsOutline /><span>Settings</span></Link></li>
-                    <li><Link to={"/logout"}><IoLogOutOutline /><span>Logout</span></Link></li>
+                    {/* <li><Link to={"/settings"}><IoSettingsOutline /><span>Settings</span></Link></li> */}
+                    <li><Link onClick={() => handleLogOut()}><IoLogOutOutline /><span>Logout</span></Link></li>
                 </ul>
             </nav>
         </aside>
