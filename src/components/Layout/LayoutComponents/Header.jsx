47947412c9a68dd4a3e94d277f5ad0ba5ec32ae0
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-10-27 20:13:20 +0100

    feat(header): add functionality to save favorite stocks, update user avatar

--- src/components/Layout/LayoutComponents/Header.jsx
+++ src/components/Layout/LayoutComponents/Header.jsx
@@ -13,18 +13,27 @@ import {
   InputGroup,
   InputLeftElement,
   Alert,
-  AlertIcon
+  AlertIcon,
+  Heading
 } from "@chakra-ui/react"
-import { useContext } from "react"
-import { IoLogOutOutline, TbZoomMoney } from "react-icons/all"
+import { useContext, useState } from "react"
+import { FaHeart, FaRegHeart, IoLogOutOutline, TbZoomMoney } from "react-icons/all"
 import { DataContext } from "../../../context/data.context"
 import styles from "../Layout.module.css"
 import { stocksArr } from "../../../utils/seeds/stocksArr"
 import StockTable from "../../Stocks/StocksComponents/StockTable"
 
 function Header() {
-  
-  const { user, searchResults, setSearchResults, isSearching, setIsSearching } = useContext(DataContext)
+  const {
+    user,
+    searchResults,
+    setSearchResults,
+    isSearching,
+    setIsSearching,
+    watchlist
+  } = useContext(DataContext)
+  const [isWatchlistOpen, setIsWatchlistOpen] = useState(false)
+
   const handleLogOut = () => {
     return window.location.reload()
   }
@@ -46,6 +55,10 @@ function Header() {
     }
   }
 
+  const handleOpenWatchlist = () => {
+    setIsWatchlistOpen(!isWatchlistOpen)
+  }
+
   return (
     <Box className={styles.header}>
       <Box p='0 20px' w='100%' display='flex' justifyContent='space-between' >
@@ -55,51 +68,83 @@ function Header() {
               pointerEvents='none'
               children={<TbZoomMoney size='25px' color='#6e6e6e' />}
             />
-            <Input id="searchBar" onChange={(e) => { handleSearchStock(e.target.value) }} type='text' placeholder='Search Stocks' />
+            <Input shadow='0.6px 0.6px 1px #ccc' id="searchBar" onChange={(e) => { handleSearchStock(e.target.value) }} type='text' placeholder='Search Stocks' />
           </InputGroup>
-          {searchResults.length <= 0
-            ? <Box
-              fontWeight='500'
-              position='fixed'
-              top='70px'
-              zIndex='1'
-              w='400px'>{isSearching
-                ? <Alert status='warning'>
-                  <AlertIcon />
-                  Stock not found.
-                </Alert>
-                : null}</Box>
-            : <Box
-              w='fit-content'
-              position='fixed'
-              top='70px'
-              zIndex='1'
-              maxH='500px'
-              overflow='scroll'
-              overflowX='hidden'
-            >
-              <StockTable data={searchResults} title='Results' />
-            </Box>
-          }
-        </Box>
-        <Menu>
-          <MenuButton
-            as={Button}
-            variant='unstyled'
-            cursor='pointer'
+          <Box
+            minW='300px'
+            position='fixed'
+            top='70px'
+            zIndex='1'
+            maxH='500px'
+            overflow='scroll'
+            overflowX='hidden'
+            borderRadius={8} shadow='0 0 4px #ccc'
           >
-            <Box display='flex' justifyContent='right' alignItems='center' gap='10px'>
-              <Text textTransform='capitalize' fontWeight='500'>{user.userName.length > 10 ? user.userName.slice(0, 9) + '...' : user.userName}</Text>
-              <Avatar boxSize='35px' className='avatar'><AvatarBadge boxSize='1em' bg='green.500' /></Avatar>
+            {
+              searchResults.length < 1
+                ? isSearching && <Alert status='warning'><AlertIcon />Stock not found.</Alert>
+                : <StockTable data={searchResults} title='Results' />
+            }
+          </Box>
+        </Box>
+        <Box display='flex' gap='30px'>
+
+          <Box>
+            <Button
+              onClick={() => handleOpenWatchlist()}
+              variant='ghost'
+            >
+              {watchlist.length > 0 ? <FaHeart fontSize='1.3rem' /> : <FaRegHeart fontSize='1.3rem' />}
+              <Text
+                fontSize='0.7rem'
+                position='fixed'
+                marginTop='1px'
+                color='#fff'
+                zIndex={1}
+              >{watchlist.length}
+              </Text>
+            </Button>
+            <Box
+
+              display={isWatchlistOpen === true ? 'block' : 'none'}
+              position='fixed'
+              top='70px'
+              right='140px'
+              zIndex={1}
+            >
+              {
+                watchlist.length < 1
+                  ? <Box className='box'>
+                    <Heading textAlign='left' marginBottom='20px' >Watchlist</Heading>
+                    <Alert status='warning' borderRadius={8} display='flex' flexDirection='column' padding='30px' gap='10px' fontWeight='500'>
+                      <FaRegHeart fontSize='30px' />
+                      Your Watchlist is empty.<br /> Please, add the stocks you want to track.
+                    </Alert>
+                  </Box>
+                  : <StockTable data={watchlist} type='watchlist' title='Watchlist' />
+              }
             </Box>
-          </MenuButton>
-          <MenuList className={styles.menuList}>
-            {/* <MenuItem >Profile</MenuItem>
+          </Box>
+          <Menu>
+            <MenuButton
+              as={Button}
+              variant='unstyled'
+              cursor='pointer'
+            >
+              <Box display='flex' justifyContent='right' alignItems='center' gap='10px'>
+                <Text textTransform='capitalize' fontWeight='500'>{user.userName.length > 10 ? user.userName.slice(0, 9) + '...' : user.userName}</Text>
+                <Avatar boxSize='35px' className='avatar'><AvatarBadge boxSize='1em' bg='green.500' /></Avatar>
+              </Box>
+            </MenuButton>
+            <MenuList className={styles.menuList}>
+              {/* <MenuItem >Profile</MenuItem>
             <MenuItem >Account</MenuItem>
             <MenuDivider /> */}
-            <MenuItem icon={<IoLogOutOutline fontSize='1.5em' />} onClick={() => handleLogOut()}>Logout</MenuItem>
-          </MenuList>
-        </Menu>
+              <MenuItem icon={<IoLogOutOutline fontSize='1.5em' />} onClick={() => handleLogOut()}>Logout</MenuItem>
+            </MenuList>
+          </Menu>
+        </Box>
+
       </Box>
     </Box>
   )
