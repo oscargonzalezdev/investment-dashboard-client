e323e2970c441ff4a10b819626b3d5e1dd78955f
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-10-27 20:28:52 +0100

    feat(investments): add investments component to display bought and sold stocks

---
+++ src/components/Investments/Investments.jsx
@@ -0,0 +1,25 @@
+import { Box, Alert, Heading, AlertIcon } from "@chakra-ui/react"
+import { useContext } from "react"
+import { DataContext } from "../../context/data.context"
+import StockTable from '../Stocks/StocksComponents/StockTable'
+
+function Investments() {
+    const { investments } = useContext(DataContext)
+    return (
+        <Box className='main-container'>
+            {
+                investments.length < 1
+                    ? <Box className='box'>
+                        <Heading className='section-title'>Last Trades</Heading>
+                        <Alert status='warning' borderRadius={8} display='flex' flexDirection='column' padding='30px' gap='10px' fontWeight='500'>
+                            <AlertIcon fontSize='40px' />
+                            Your Trade list is empty.<br />
+                        </Alert>
+                    </Box>
+                    : <StockTable data={investments} type='investments' title='Last Trades' />
+            }
+        </Box>
+    )
+}
+
+export default Investments
