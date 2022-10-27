afb81f4d6aa1fa4f8da27bb791cecee5e9b49b83
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-10-27 19:57:52 +0100

    feat(style): add custom colors to chakra theme

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
+import { DataProviderWrapper } from './context/data.context'
 
+const colors = {
+  brand: {
+    100: '#D0EDFA',
+    200: '#A5DFF9',
+    300: '#7AD1F8',
+    400: '#4FC3F7',
+    500: '#3F51B5',
+    600: '#00BCD4',
+    700: '#478AD6',
+    800: '#436EC6',
+    900: '#3F51B5'
+  },
+}
+
+const theme = extendTheme({ colors })
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <DataProviderWrapper>
+        <App />
+      </DataProviderWrapper>
+    </ChakraProvider>
   </React.StrictMode>
 )
