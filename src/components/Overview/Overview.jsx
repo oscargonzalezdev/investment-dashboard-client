b19f8bd8262cb7be7bf34b2e27d6b5df2b141716
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-11-02 11:43:00 +0000

    feat(style): add css module to style overview component

--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -6,18 +6,19 @@ import PieChart from "../Charts/PieChart"
 import BreakingNews from "../News/BreakingNews"
 import StockDetails from "../Stocks/StocksComponents/StockDetails"
 import StockTable from "../Stocks/StocksComponents/StockTable"
+import styles from "./Overview.module.css"
 
 function Overview() {
     const { trendingStocks } = useContext(DataContext)
 
     return (
         <>
-            <Grid templateColumns='repeat(9, 1fr)' gap='20px'>
-                <GridItem display='flex' flexDirection='column' gap='20px' colSpan={5}>
+            <Grid className={styles.manGrid}>
+                <GridItem className={styles.leftGrid}>
                     <StockDetails w='100%' title='Stock of the Week' symbol2={'IBM'} />
                     <StockTable w='100%' h='400px' data={trendingStocks} type='trending' title='Trending Stocks' />
                 </GridItem>
-                <GridItem display='flex' flexDirection='column' gap='20px' colSpan={4}>
+                <GridItem className={styles.rightGrid}>
                     <Box display='flex' gap='20px'>
                         <Balance />
                         <PieChart />
